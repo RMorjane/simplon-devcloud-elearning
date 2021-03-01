452e8ee2f2849ab988a9bf573f7f1ab7f00b890c
RHELLAB Morjane <rhellab_morjane@yahoo.fr>
2021-03-01 10:59:28 +0100

    update 2 main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from elearning import MyElearning
+from flask import Flask, request, render_template, jsonify
+import logging
+import json
 
-if __name__ == "__main__":
+logging.basicConfig(filename='logs.log',level=logging.DEBUG)
 
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def get_video():
     learning = MyElearning()
     learning.set_logger()
     learning.connect()
-    critere_recherche = {"video_name": "cloud"} # exemple de critère de recherche des vidéos
-    learning.find_videos(critere_recherche)
-    print(learning.list_videos)
+    learning.read_videos()
+    a = learning.list_videos
+    return render_template("index.html",video = a)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=3000, debug=True)
