79338329c6b1f07c2207fa91f7b218ccf1bb656a
RHELLAB Morjane <rhellab_morjane@yahoo.fr>
2021-03-16 01:17:54 +0100

    update elearning web app

--- elearning.py
+++ elearning.py
@@ -1,4 +1,6 @@
 import psycopg2
+import logging    
+from logging.handlers import RotatingFileHandler
 
 class MyElearning:
 
@@ -6,18 +8,59 @@ class MyElearning:
         self.connection = None
         self.list_videos = []
         self.list_vcategories = []
+        self.logger = None
 
     def connect(self):
         try:
             self.connection = psycopg2.connect(
-                host = "test_postgres",
+                host = "dbpostgres",
                 user = "postgres",
-                password = "test",
+                password = "postgres",
                 port = "5432"
             )
-            print("Connexion réussie : ",self.connection)
+            self.logger.info("Connexion réussie : " + str(self.connection))
+            return True
         except (Exception, psycopg2.Error) as error:
-            print("Impossible de se connecter au serveur postgres : ",error)
+            self.logger.error("Impossible de se connecter au serveur postgres : " + str(error))
+            return False
+
+    def create_tables(self):
+        try:
+            with self.connection.cursor() as my_cursor:
+                sql_create_tables = """
+                create table if not exists vcategory(
+                vcategory_id serial primary key,
+                vcategory_name varchar(50) not null
+                );
+
+                create table if not exists video(
+                video_id serial primary key,
+                video_name varchar(100) not null,
+                video_link varchar(200) not null,
+                vcategory_id int references vcategory(vcategory_id)
+                );
+
+                insert into vcategory(vcategory_name)
+                values('cloud'),('technology'),('development');
+
+                insert into video(video_name,video_link,vcategory_id)
+                values('Le cloud computing expliqué en 7 minutes','https://www.youtube.com/embed/UC5cs06DgLFeyLIF_II7lWCQ',1),
+                ('XAVKI','https://www.youtube.com/embed/UC5cs06DgLFeyLIF_II7lWCQ',1),
+                ('Top 10 Technologies to Learn in 2021 | Trending Technologies in 2021 | Edureka','https://www.youtube.com/embed/UC5cs06DgLFeyLIF_II7lWCQ',2),
+                ('i bought a DDoS attack on the DARK WEB (dont do this)','https://www.youtube.com/embed/UC5cs06DgLFeyLIF_II7lWCQ',2),
+                ('Raspberry Pi Explained in 100 Seconds','https://www.youtube.com/embed/UC5cs06DgLFeyLIF_II7lWCQ',2),
+                ('Programming tutorials','https://www.youtube.com/embed/UC5cs06DgLFeyLIF_II7lWCQ',3),
+                ('Kubernetes Tutorial for Beginners [FULL COURSE in 4 Hours]','https://www.youtube.com/embed/UC5cs06DgLFeyLIF_II7lWCQ',1),
+                ('LES BASES DE GIT (tuto débutant)','https://www.youtube.com/embed/UC5cs06DgLFeyLIF_II7lWCQ',3);
+                """
+                my_cursor.execute(sql_create_tables)
+                self.connection.commit()
+                my_cursor.close()
+                self.logger.info("création des tables réussie !!!")
+                return True
+        except (Exception, psycopg2.Error) as error:
+            self.logger.error("Impossible de créer les tables dans la base postgres : " + str(error))
+            return False
 
     def read_videos(self):
         try:
@@ -32,8 +75,9 @@ class MyElearning:
                         "vcategory_id": video[3]
                     })
                 my_cursor.close()
+                self.logger.info("Reading videos successfully!!!")
         except (Exception, psycopg2.Error) as error:
-            print("Erreur dans la requête de selection des videos : ",error)
+            self.logger.error("Erreur dans la requête de selection des videos : "+str(error))
 
     def add_video(self,video_name,video_link,vcategory_id):
         try:
@@ -43,15 +87,16 @@ class MyElearning:
                 my_cursor.execute(sql_add_video %(video_name,video_link,vcategory_id))
                 self.connection.commit()
                 my_cursor.close()
-                video_id = len(list_videos)+1
+                video_id = len(self.list_videos)+1
                 self.list_videos.append({
                     "video_id": video_id,
                     "video_name": video_name,
                     "video_link": video_link,
                     "vcategory_id": vcategory_id
                 })
+                self.logger.info("Adding video successfully!!!")
         except (Exception, psycopg2.Error) as error:
-            print("Erreur dans la requête d'insertion de la video : ",error)
+            self.logger.error("Erreur dans la requête d'insertion de la video : "+str(error))
 
     def get_video_id(self,video_link):
         with self.connection.cursor() as my_cursor:
@@ -101,8 +146,9 @@ class MyElearning:
                         "vcategory_id": video[3]
                     })
                 my_cursor.close()
+                self.logger.info("Successfull : Videos was founded")
         except (Exception, psycopg2.Error) as error:
-            print("Erreur dans la requête de selection des videos : ",error)
+            self.logger.error("Erreur dans la requête de selection des videos : "+str(error))
 
     def read_vcategories(self):
         try:
@@ -115,8 +161,9 @@ class MyElearning:
                         "vcategory_name": vcategory[1]
                     })
                 my_cursor.close()
+                self.logger.info("Readding video's categories successfully!!!")
         except (Exception, psycopg2.Error) as error:
-            print("Erreur dans la requête de selection des catégories de videos : ",error)
+            self.logger.error("Erreur dans la requête de selection des catégories de videos : "+str(error))
 
     def add_vcategory(self,vcategory_name):
         vcategory_id = self.get_vcategory_id(vcategory_name)
@@ -132,8 +179,9 @@ class MyElearning:
                         "vcategory_id": vcategory_id,
                         "vcategory_name": vcategory_name
                     })
+                    self.logger.info("Adding video category successfully!!!")
             except (Exception, psycopg2.Error) as error:
-                print("Erreur dans la requête d'insertion de la catégorie de videos : ",error)
+                self.logger.error("Erreur dans la requête d'insertion de la catégorie de videos : "+str(error))
         return vcategory_id
 
     def get_vcategory_id(self,vcategory_name):
@@ -143,3 +191,12 @@ class MyElearning:
             vcategory_id = my_cursor.fetchone()
             my_cursor.close()
             return vcategory_id
+
+    def set_logger(self):
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.DEBUG)        
+        formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+        file_handler = RotatingFileHandler('log.txt', 'a', 1000000, 1)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from elearning import MyElearning
+from flask import Flask, request, render_template, jsonify
+import logging
+import json
+
+logging.basicConfig(filename='logs.log',level=logging.DEBUG)
+
+learning = MyElearning()
+learning.set_logger()
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def get_video():
+    name = request.args.get("video")
+    if name == None:
+        learning.read_videos()
+    else:
+        learning.find_videos({
+            "video_name": str(name)
+        })
+    a = learning.list_videos
+    return render_template("index.html",video = a)
+
+@app.route('/video/<video_id>',methods=['GET'])
+def video_page(video_id):
+    learning.find_videos({
+        "video_id": int(video_id)
+    }) 
+    list_videos = learning.list_videos
+    a = list_videos[0]
+    learning.read_videos()
+    b = learning.list_videos
+    print("-----------")
+    return render_template("see_video.html", video = a, video_all = b)
 
 if __name__ == "__main__":
-    learning = MyElearning()
-    learning.connect()
-    critere_recherche = {"video_name": "cloud"} # exemple de critère de recherche des vidéos
-    learning.find_videos(critere_recherche)
-    print(learning.list_videos)
+    if learning.connect() and learning.create_tables():
+        app.run(host="0.0.0.0",port=3000, debug=True)
